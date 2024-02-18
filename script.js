e2831fee3a87e6c8c5a4bc0f0cdf0c0d82a3d0e4
Suresh Patil <patilrsuresh@outlook.com>
2024-02-18 08:50:51 -0500

    first commit of SBA

---
+++ script.js
@@ -0,0 +1,36 @@
+
+//Task1: Declare variables properly using let and const where appropriate.
+
+const maxAttempts = 3;
+const companyName = "ABCltd"
+let certified = true;
+let marketCap = 433;
+let locations = 15;
+let locationperCap;
+locationperCap = marketCap / locations;
+
+//-------------------------------------------------------------------------------------
+//Task2: Use operators to perform calculations on variables and literals
+// Declare variables
+let x = 523;
+let y = 481;
+
+// Perform calculations
+let sum = x + y;
+let difference = x - y;
+let product = x * y;
+let quotient = x / y;
+let remainder = x % y;
+
+console.log("Sum:", sum);
+console.log("Difference:", difference);
+console.log("Product:", product);
+console.log("Quotient:", quotient);
+console.log("Remainder:", remainder);
+console.log(`the addition of ${x} and ${y} is ${sum}`)
+
+//Use operators to perform calculations on variables and literals.
+
+
+
+
